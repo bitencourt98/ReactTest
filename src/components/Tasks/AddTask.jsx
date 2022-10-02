cc17c6ddba825530971385e5e37c19e1a3670aa3
bitencourt98 <renancosta1998@gmail.com>
2022-10-02 15:15:12 -0300

    added button to receive input value

--- src/components/Tasks/AddTask.jsx
+++ src/components/Tasks/AddTask.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./AddTask.css"
 import Button from './Button';
 
 const AddTask = () => {
+    const [inputData, setInputData] = useState("");
+
+    const handleInputChange = (e) => {
+        console.log(e.target.value);
+        setInputData(e.target.value);
+    }
+
     return (
         <div className='add-task-container'>
-            <input className="add-task-input" type="text" />
+            <input onChange={handleInputChange} value={inputData} className="add-task-input" type="text" />
             <div className="add-task-button-container">
                 <Button>Adicionar</Button>
             </div>
