2734d2613a74e447a7d924668fe11248072a590b
bitencourt98 <renancosta1998@gmail.com>
2022-10-02 04:19:34 -0300

    added button component and style

--- src/components/Tasks/AddTask.jsx
+++ src/components/Tasks/AddTask.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import "./AddTask.css"
+import Button from './Button';
+
 const AddTask = () => {
-    return <input type="text" />
+    return (
+        <div className='add-task-container'>
+            <input className="add-task-input" type="text" />
+            <div className="add-task-button-container">
+                <Button>Adicionar</Button>
+            </div>
+        </div>
+    )
 }
  
 export default AddTask;
